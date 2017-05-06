b5c8a70dcca656d0ab03ad34a5a86b3a968d122a
Gary Wong <gary.wonghg@gmail.com>
2017-05-06 16:06:42 -0700

    (init) Migrate JobEntry Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react'
+import React, { PropTypes, Component } from 'react';
+import Dialog from 'material-ui/Dialog';
 
-export default class App extends Component {
-  constructor (props) {
-    super(props)
-  }
+const propTypes = {
+  jobPost: PropTypes.object.isRequired,
+};
 
-  componentWillMount () {
-  }
-
-  render () {
-    return (
-      <div>
-        <h1> hello world </h1>
-      </div>
-    )
-  }
+export default JobInfo = (jobPost) => {
+  const { comapny,
+          job_title,
+          job_description,
+          basic_qualifications,
+          preferred_qualifications,
+          location,
+          posted_date,
+          posted_url,
+        } = jobPost;
+  return (
+    <div>
+      <h1>Title</h1>
+      { job_title } at { company }
+      <h1>Location</h1>
+      { location }
+      <h1>Description</h1>
+      { job_description }
+      <h1>Basic Qualifications</h1>
+      { basic_qualifications }
+      <h1>Preferred Qualifications</h1>
+      { preferred_qualifications }
+      <h1>Job Posting Url</h1>
+      { post_url }
+    </div>
+  )
 }
