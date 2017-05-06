536ef8a12ccd53a1c6dee9048145fc7b1649289f
Gary Wong <gary.wonghg@gmail.com>
2017-05-06 16:24:14 -0700

    (fix) Re-fix App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,34 +1,18 @@
-import React, { PropTypes, Component } from 'react';
-import Dialog from 'material-ui/Dialog';
+import React, { Component } from 'react'
 
-const propTypes = {
-  jobPost: PropTypes.object.isRequired,
-};
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+  }
 
-export default JobInfo = (jobPost) => {
-  const { comapny,
-          job_title,
-          job_description,
-          basic_qualifications,
-          preferred_qualifications,
-          location,
-          posted_date,
-          posted_url,
-        } = jobPost;
-  return (
-    <div>
-      <h1>Title</h1>
-      { job_title } at { company }
-      <h1>Location</h1>
-      { location }
-      <h1>Description</h1>
-      { job_description }
-      <h1>Basic Qualifications</h1>
-      { basic_qualifications }
-      <h1>Preferred Qualifications</h1>
-      { preferred_qualifications }
-      <h1>Job Posting Url</h1>
-      { post_url }
-    </div>
-  )
+  componentWillMount () {
+  }
+
+  render () {
+    return (
+      <div>
+        <h1> hello world </h1>
+      </div>
+    )
+  }
 }
